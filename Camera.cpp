faf791680752bc791c72aef9345933f17a21645c
pork3 <zbower1@gmail.com>
2019-02-17 16:15:49 -0800

    fixed incorrect perspective values

--- Camera.cpp
+++ Camera.cpp
@@ -6,13 +6,14 @@ Camera::Camera(const glm::vec3 &pos, float fov, float aspect, float near, float 
     /*calculate with function?*/
     this->position = pos;
 
-
+    /******* HARD CODED VALUES CHANGE ******/
     this->forward = glm::vec3(0,0,-1);
     this->up = glm::vec3(0,1,0);
 }
 
 glm::mat4 Camera::GetProjection() const {
 
+    /*get the perspective the camera should have based on current position and forward and up*/
     return perspective * glm::lookAt(position, position + forward , up);
 
 }
