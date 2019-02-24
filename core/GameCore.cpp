d788649dad54bb471b2c33381b14ec1bdcf98f26
pork3 <zbower1@gmail.com>
2019-02-24 00:24:48 -0800

    added error logging

---
+++ core/GameCore.cpp
@@ -0,0 +1,5 @@
+//
+// Created by zach on 2/23/19.
+//
+
+#include "GameCore.h"
---
+++ core/GameCore.h
@@ -0,0 +1,14 @@
+//
+// Created by zach on 2/23/19.
+//
+
+#ifndef ENGINE_GAMECORE_H
+#define ENGINE_GAMECORE_H
+
+
+class GameCore {
+
+};
+
+
+#endif //ENGINE_GAMECORE_H
